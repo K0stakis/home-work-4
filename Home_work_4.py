f3d63257bc4490eaabd1d0dcedf0ed3ae435c245
K0stakis <141132210+K0stakis@users.noreply.github.com>
2023-08-13 11:22:53 +0300

    Add files via upload

---
+++ Home_work_4.py
@@ -0,0 +1,34 @@
+﻿# 1) Дана довільна строка. Напишіть код, який знайде в ній і віведе на екран кількість слів, які містять дві голосні літери підряд.
+print("First task:")
+
+while True:
+    user_input = input('Write a proposal: ')
+    if user_input.isdigit():
+        print("No need to write numbers here! ")
+    else:
+        break
+
+vowels = "AEIOUYaeiouy"
+
+words = user_input.split()
+
+count_words_with_double_vowels = 0
+
+for word in words:
+    for double_vowels in range(len(word)):
+        if word[double_vowels] in vowels and word[double_vowels+1] in vowels:
+            count_words_with_double_vowels += 1
+            break  
+
+print(count_words_with_double_vowels)
+
+# 2) Є два довільних числа які відповідають за мінімальну і максимальну ціну. Є Dict з назвами магазинів і цінами: { "cito": 47.999, "BB_studio" 42.999, "momo": 49.999, "main-service": 37.245, "buy.now": 38.324, "x-store": 37.166, "the_partner": 38.988, "store": 37.720, "rozetka": 38.003}. Напишіть код, який знайде і виведе на екран назви магазинів, ціни яких попадають в діапазон між мінімальною і максимальною ціною. 
+print("Second task:")
+
+dict = { "cito": 47.999, "BB_studio": 42.999, "momo": 49.999, "main-service": 37.245, "buy.now": 38.324, "x-store": 37.166, "the_partner": 38.988, "store": 37.720, "rozetka": 38.003}
+min = 37.166
+max = 49.999
+            
+for dict_key, dict_value in dict.items():
+    if min < dict_value < max :
+        print(dict_key)
